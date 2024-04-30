17d6015795712752b9e3529905023c2d92b7175f
Damon McCullough <damonmcc1391@gmail.com>
2024-04-30 11:50:21 -0400

    format with prettier

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,45 +4,51 @@ import * as d3 from "d3";
 
 export function simpleMap(div_name, forecastArea) {
   var divMap = L.DomUtil.get(div_name);
-    if(divMap != null){
-      divMap._leaflet_id = null;
-    }
+  if (divMap != null) {
+    divMap._leaflet_id = null;
+  }
   const forecastMap = L.map(divMap);
-  L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-    minZoom: 0,
-    maxZoom: 18,
-    ext: 'png'
-  }).addTo(forecastMap);
+  L.tileLayer(
+    "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}",
+    {
+      minZoom: 0,
+      maxZoom: 18,
+      ext: "png",
+    }
+  ).addTo(forecastMap);
   forecastArea.addTo(forecastMap);
   forecastMap.fitBounds(forecastArea.getBounds());
   forecastMap.setZoom(9);
 
-  return forecastMap
+  return forecastMap;
 }
 
-export function temperaturePlot(forecast, {width} = {}) {
+export function temperaturePlot(forecast, { width } = {}) {
   let forecastPeriods = forecast.properties.periods;
-  var meanTemp = Math.round(d3.mean(forecastPeriods.map(a => a.temperature)));
+  var meanTemp = Math.round(d3.mean(forecastPeriods.map((a) => a.temperature)));
   return Plot.plot({
     title: "Hourly temperature forecast",
     width,
-    x: {type: "utc", ticks: "day", label: null},
-    y: {grid: true, inset: 10, label: "Degrees (F)"},
+    x: { type: "utc", ticks: "day", label: null },
+    y: { grid: true, inset: 10, label: "Degrees (F)" },
     color: {
       type: "diverging",
-      scheme: "burd", 
-      legend: true, 
+      scheme: "burd",
+      legend: true,
       pivot: meanTemp,
-      symmetric: true
+      symmetric: true,
     },
     marks: [
       // Plot.ruleY(meanTemp),
       Plot.dot(
-        forecastPeriods, {
-          x: "startTime", 
-          y: "temperature", 
-          fill: "temperature"},
-      Plot.crosshairX(forecastPeriods, {x: "startTime", y: "temperature"})),
-  ]
+        forecastPeriods,
+        {
+          x: "startTime",
+          y: "temperature",
+          fill: "temperature",
+        },
+        Plot.crosshairX(forecastPeriods, { x: "startTime", y: "temperature" })
+      ),
+    ],
   });
 }
